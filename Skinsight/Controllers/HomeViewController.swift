e3deab893165ec085e415112a254014580089ca7
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-08-02 09:57:13 -0600

    ADD Auth0 login and logout

--- Skinsight/Controllers/HomeViewController.swift
+++ Skinsight/Controllers/HomeViewController.swift
@@ -36,33 +36,10 @@ class HomeViewController: UIViewController {
         signInButton.translatesAutoresizingMaskIntoConstraints = false // Important to use Auto Layout
         
         // Button actions
-//        signInButton.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
         signInButton.addTarget(self, action: #selector(signInButtonTouchDown), for: .touchDown)
         signInButton.addTarget(self, action: #selector(signInButtonTapped), for: .touchUpInside)
         
         self.view.addSubview(signInButton) // Add the button to the screen
-        
-        if isAuthenticated {
-            performSegue(withIdentifier: "HomeToTabSegue", sender: self)
-        }
-        
-
-        // Sign Up Button
-//        signUpButton.setTitle("Sign up", for: .normal)
-//        signUpButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 18.0)
-//        signUpButton.setTitleColor(skinsightColor, for: .normal)
-//
-//        signUpButton.layer.cornerRadius = 15
-//        signUpButton.layer.borderColor = skinsightColor.cgColor
-//        signUpButton.layer.borderWidth = 1
-//
-//        signUpButton.translatesAutoresizingMaskIntoConstraints = false
-        
-//        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
-//        signUpButton.addTarget(self, action: #selector(signUpButtonTouchDown), for: .touchDown)
-//
-//        self.view.addSubview(signUpButton)
-        
         
         // Set Auto Layout Constraints
         NSLayoutConstraint.activate([
@@ -70,11 +47,6 @@ class HomeViewController: UIViewController {
             signInButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 50), // Distance from the top label
             signInButton.widthAnchor.constraint(equalToConstant: 150), // Width constraint
             signInButton.heightAnchor.constraint(equalToConstant: 40), // Height constraint
-            
-//            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor), // Center horizontally
-//            signUpButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 20), // Distance from the sign-in button
-//            signUpButton.widthAnchor.constraint(equalToConstant: 150), // Width constraint
-//            signUpButton.heightAnchor.constraint(equalToConstant: 40), // Height constraint
         ])
     }
     
@@ -82,32 +54,17 @@ class HomeViewController: UIViewController {
     //MARK: - Navigation
     
     @objc func signInButtonTapped() {
-        // Trigger the segue to the Sign In view controller
-//        performSegue(withIdentifier: "SignInSegue", sender: self)
+    
+        login()
         
-        Auth0
-            .webAuth()
-            .start { result in
-                
-                switch result {
-                    
-                case .failure(let error):
-                    // The user pressed "Cancel" on Universal login
-                    // or something unusal happened.
-                    print("Failed with: \(error)")
-                    
-                case .success(let credentials):
-                    self.isAuthenticated = true
-                    print("Credentials: \(credentials)")
-                    print("ID Token: \(credentials.idToken)")
-                }
-                
-            }
-    }
-
-    @objc func signUpButtonTapped() {
-        // Trigger the segue to the Sign Up view controller
-        performSegue(withIdentifier: "SignUpSegue", sender: self)
+//        print(self.isAuthenticated)
+        
+//        if self.isAuthenticated {
+//            DispatchQueue.main.async {
+//                self.performSegue(withIdentifier: "HomeToTabSegue", sender: self)
+//                print("oooouu yeahhh ouu wow chacarron owww")
+//            }
+//        }
     }
     
     
@@ -134,44 +91,20 @@ class HomeViewController: UIViewController {
     
     //MARK: - Auth0
     private func login() {
-        
-        Auth0
-            .webAuth()
-            .start { result in
-                
-                switch result {
-                    
-                case .failure(let error):
-                    // The user pressed "Cancel" on Universal login
-                    // or something unusal happened.
-                    print("Failed with: \(error)")
-                    
-                case .success(let credentials):
-                    self.isAuthenticated = true
-                    print("Credentials: \(credentials)")
-                    print("ID Token: \(credentials.idToken)")
-                }
-                
+        Auth0.webAuth().start { result in
+            switch result {
+            case .failure(let error):
+                // The user pressed "Cancel" on Universal login
+                // or something unusual happened.
+                print("Failed with: \(error)")
+                self.isAuthenticated = false // Set to false in case of failure
+            case .success(let credentials):
+                self.isAuthenticated = true // Set to true upon successful login
+                print(credentials)
+
+                // Perform the segue here, inside the success case
+                self.performSegue(withIdentifier: "HomeToTabSegue", sender: self)
             }
+        }
     }
-    
-    private func logout() {
-        
-        Auth0
-            .webAuth()
-            .clearSession { result in
-                
-                switch result {
-                     
-                case .failure(let error):
-                    print("Failed with error \(error)")
-                
-                case .success:
-                    self.isAuthenticated = false
-                    
-                }
-                
-            }
-    }
-    
 }
--- Skinsight/Controllers/ProfileViewController.swift
+++ Skinsight/Controllers/ProfileViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import Auth0
 
 // This is the controller class for the Profile screen.
 class ProfileViewController: UIViewController {
     
+    let skinsightColor = UIColor(red: 20/255.0, green: 108/255.0, blue: 148/255.0, alpha: 1.0) // #146C94 color
+    
+    let logoutButton = UIButton(type: .custom)
     var userProfile = Profile.empty
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        logoutButton.setTitle("Log out", for: .normal)
+        logoutButton.titleLabel?.font = UIFont(name: "Lato-Regular", size: 18.0)
+        
+        logoutButton.backgroundColor = skinsightColor
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Button actions
+//        logoutButton.addTarget(self, action: #selector(signInButtonTouchDown), for: .touchDown)
+        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+        
+        self.view.addSubview(logoutButton)
+        
+        // Constraints
+        NSLayoutConstraint.activate([
+            logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor), // Center horizontally
+            logoutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor) // Center vertically
+            
+        ])
+    }
+    
+    //MARK: - Navigation
+    @objc func logoutButtonTapped() {
+        
+        logout()
+        
+        self.performSegue(withIdentifier: "LogoutSegue", sender: self)
+    }
+    
+    
+    //MARK: - Auth0
+    private func logout() {
+        
+        Auth0
+            .webAuth()
+            .clearSession { result in
+                
+                switch result {
+                     
+                case .failure(let error):
+                    print("Failed with error \(error)")
+                
+                case .success:
+                    
+                    print("Eh vegetta eres el mejor youtuber")
+                }
+            }
     }
 }
